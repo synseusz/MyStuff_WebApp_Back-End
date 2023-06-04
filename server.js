1f87fcb4f4e09ef0bd4781cf7d311d9e85eefb86
Piotr Orlowski <thesynseusz@gmail.com>
2023-06-04 23:49:16 +0200

    fixes to server.js SSL

--- server.js
+++ server.js
@@ -1,57 +1,38 @@
-'use strict'
-//import express module
-const express = require('express')
-//import cors to enable cors
-const cors = require('cors')
-//import body parser
-const bodyParser = require('body-parser')
-
-const routes = require('./routes')
-
+'use strict';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 const https = require('https');
 const fs = require('fs');
 
-//create the express module
-const server = express()
-
+const app = express();
 const options = {
-	key: fs.readFileSync(process.env.SSL_KEY),
-	cert: fs.readFileSync(process.env.SSL_CERT)
+  key: fs.readFileSync(process.env.SSL_KEY),
+  cert: fs.readFileSync(process.env.SSL_CERT)
 };
-const sslServer = https.createServer(options, server);
+const sslServer = https.createServer(options, app);
 
-//To parse json data
-server.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+app.use(express.static('public'));
 
-//enable all origins
-server.use(cors())
-
-//enable cors for more complex routes
-server.options('*', cors())
-
-//allow static files to serve the images
-server.use(express.static('public'))
-
-//prepare our database connection parameters
 const databaseData = {
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env.PASSWD,
-	database: process.env.DATABASE
-}
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWD,
+  database: process.env.DATABASE
+};
 
-//save server port on global variable
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-//----- add all routes to the api end points ------
-routes.allRoutes(databaseData, server)
+routes.allRoutes(databaseData, app);
 
-//start the server
-sslServer.listen(port, err => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(`App is ready on port ${port}`);
-	}
+sslServer.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`App is ready on port ${port}`);
+  }
 });
-
